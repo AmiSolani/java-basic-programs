69917129650bede0e305728cdcf2d16e89098aff
Ami Solani <38397062+AmiSolani@users.noreply.github.com>
2018-04-15 18:55:35 +0530

    Example of StaticImport

---
+++ S.java
@@ -0,0 +1,9 @@
+import static java.lang.System.*;    
+class Staticimportexample{  
+  public static void main(String args[]){  
+     
+   out.println("Hello");  
+   out.println("Java");  
+  
+ }   
+} 
