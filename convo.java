a3cbdea1f00c0fffd534fa203e8d4165f9da59e6
Ami Solani <38397062+AmiSolani@users.noreply.github.com>
2018-04-15 17:27:25 +0530

    Example for Rs. to Dollar conversion.

---
+++ convo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class convo 
+{
+	public static void main(String args[])
+	{
+		Scanner ip=new Scanner(System.in);
+		System.out.print("Enter value in Rs: ");
+		int s1=ip.nextInt();
+		int dol=s1/60;
+		System.out.println("value in dollar:"+  dol);
+	}
+}
