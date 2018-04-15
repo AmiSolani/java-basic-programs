c36ee509cdab800f96e34c08811c8a62ea31bb66
Ami Solani <38397062+AmiSolani@users.noreply.github.com>
2018-04-15 18:37:31 +0530

    pattern printing
    55555
    4444
    333
    22
    1

---
+++ p123.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+ class p123 {
+
+    public static void main(String[] args) {
+    	int i,j,k;
+
+    	 for (i=5;i>=1 ;i-- ) {
+    	   // for (k=5;k>i ;k--) {
+    	   // 	System.out.print(k);
+    	    	
+    	   // }
+    	 
+    		for (j=1;j<=i ;j++) {
+    		
+    				System.out.print(i);
+  
+    		}
+
+    		System.out.println();
+
+       	}
+    }
+}
