8aefa1f0a7057301601ff38371ef88180b3b7de7
Ami Solani <38397062+AmiSolani@users.noreply.github.com>
2018-04-15 17:58:08 +0530

    Example for StringBuffer Operation part 2

---
+++ Example31.java
@@ -0,0 +1,9 @@
+public class Example31
+{
+   public static void main(String args[])
+   {
+           StringBuffer  str= new StringBuffer  ("This is a JAVA");
+           System.out.println(str.append("Program"));
+           System.out.println(str.insert(9,"Advance"));   
+    }
+} 
