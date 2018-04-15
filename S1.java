15b8aa4e09c0eda772cbd7376492a81e92479e2f
Ami Solani <38397062+AmiSolani@users.noreply.github.com>
2018-04-15 18:58:20 +0530

    Example 2 of StaticImport

---
+++ S1.java
@@ -0,0 +1,29 @@
+package test;
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
+
+public class S1 {
+
+     public static void main(String args[]) {
+       
+        
+         System.out.println("Maximum value of int variable in Java without " + 
+                     "static import : "  + Integer.MAX_VALUE);
+
+         System.out.println("Minimum value of int variable in Java without " +
+
+                            static import : " + Integer.MIN_VALUE);
+       
+
+         System.out.println("Maximum value of int variable using " +
+                            static import : " + MAX_VALUE);
+
+         System.out.println("Minimum value of int variable using" +
+
+                            static import : " + MIN_VALUE);
+     }
+}
+
+
+
+
