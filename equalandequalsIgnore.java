4d4ba2514a2efd80fa8cf862298ba15286db0497
Ami Solani <38397062+AmiSolani@users.noreply.github.com>
2018-04-15 17:44:44 +0530

    Example for equls and equlasIgnorecase in java.

---
+++ equalandequalsIgnore.java
@@ -0,0 +1,17 @@
+public class equal1
+{    
+	public static void main(String args[])
+	{
+	String s1="hello";     
+	String s2="Hello";  
+		if (s1.equals(s2))
+	   	{
+	   		System.out.println("String are equal");
+	   	}
+	   	if (s1.equalsIgnoreCase(s2))
+	   	{
+	   		System.out.println("String are equal");	
+	   	}	        
+	    
+	}
+}   
