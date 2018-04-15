7f5b87dc700821aa84b2aaa1c1416bccfbb8feb0
Ami Solani <38397062+AmiSolani@users.noreply.github.com>
2018-04-15 17:47:54 +0530

    Example for compareTo and CompareToIgnorecase

---
+++ Example1.java
@@ -0,0 +1,11 @@
+public class Example1
+{ 
+  public static void main(String args[])
+  {   
+  String s1="hello";  
+  String s2="hello";
+  String s3= "Hello";
+  System.out.println(s1.compareTo(s2));
+  System.out.println(s1.compareToIgnoreCase(s3));   
+  } 
+}  
