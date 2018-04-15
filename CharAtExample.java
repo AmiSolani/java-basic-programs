3ea42604e594d429b6ca5a41c960cffa04ed95e2
Ami Solani <38397062+AmiSolani@users.noreply.github.com>
2018-04-15 17:13:53 +0530

    how to use CharAt in java.

---
+++ CharAtExample.java
@@ -0,0 +1,11 @@
+public class CharAtExample 
+{     
+public static void main(String args[]) 
+	{        
+ 	String str = "Welcome to string handling tutorial";  
+ 	char ch1 = str.charAt(0);  char ch2 = str.charAt(5);        
+   	System.out.println("length is "+str.length());     
+   	System.out.println("Character at 0 index is: "+ch1);  
+    	System.out.println("Character at 5th index is: "+ch2); 
+    }
+} 
