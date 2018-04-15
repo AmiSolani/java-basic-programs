6820cc763d3f56aa36b3636d63419de430ae4992
Ami Solani <38397062+AmiSolani@users.noreply.github.com>
2018-04-15 18:41:50 +0530

    package demo

---
+++ packclass1.java
@@ -0,0 +1,13 @@
+package pack1;
+public class packclass1
+{
+	public packclass1()
+	{
+		System.out.println("packclass");
+	}
+
+	public void show()
+  	{
+ 	System.out.println(" I am in pack1 package");
+ 	}
+}
