e540b988ffb0ca4cb75c283cee01dbf1e192c95c
Ami Solani <38397062+AmiSolani@users.noreply.github.com>
2018-04-15 17:09:36 +0530

    To Print Character array in java.

---
+++ chararr.java
@@ -0,0 +1,12 @@
+public class chararr
+{    
+	public static void main(String args[])
+	{     
+	String s1="hello";  
+	char[] ch=s1.toCharArray();
+	for(int i=0;i<ch.length;i++) 
+	 {              
+	 System.out.print(ch[i]);   
+	 } 
+	} 
+} 
